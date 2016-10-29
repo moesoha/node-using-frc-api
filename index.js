6f40d9cc931c77071dee2fce8c488a478d86f003
Soha King <sk@lohu.info>
2016-10-29 16:53:26 +0800

    add sth

---
+++ index.js
@@ -0,0 +1,18 @@
+/*=============================================
+Using FRC API
+-----------------------------------------------
+Tianhai Information Technology(T.H.I.T.)
+	http://tianhai.info/
+First Lemon(TrueMoe)
+	https://6385true.moe/
+Soha King
+	https://soha.moe/
+=============================================*/
+
+var request=require('request');
+
+var api=function (apiVersion,username,password,season){
+
+};
+
+module.exports=api;
